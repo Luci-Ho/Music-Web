f7bdcd3aa2d21c88fccfb257b0cb4e5e697276c2
minhquang9909 <minhquang9909@gmail.com>
2025-10-17 13:30:46 +0700

    Discover page: Use D2Card component for genre cards

--- src/components/common/D2Card.jsx
+++ src/components/common/D2Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './D2Card.css';
 
-const D2Card = ({ title = 'Title', img, className = '', onClick }) => {
+export const D2Card = ({ title = 'Title', img, className = '', onClick }) => {
 	return (
 		<div className={`d2card ${className}`} onClick={onClick} role={onClick ? 'button' : undefined} tabIndex={onClick ? 0 : undefined}>
 			<div className="d2card-thumb">
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Dashboard from '../components/layout/Dashboard';
-import '../style/Layout.css';
+import TopBar from '../components/layout/TopBar.jsx';
+import Footer from '../components/layout/Footer.jsx';
+import D2Card from '../components/common/D2Card.jsx';
 import '../App.css';
 
 const Discover = () => {
@@ -9,8 +11,24 @@ const Discover = () => {
         <div className="body">
             <div style={{ display: 'flex', width: '100%' }}>
                 <Dashboard />
-                <div>
-                    
+                <div className="container">
+                    <TopBar />
+
+                    <div className="content">
+                        <p>hello</p>
+                    </div>
+
+                    <Footer />
+
+                </div>
+
+                <div className='Genres'>
+                    <h1 className="tag">Music <span>Genres</span></h1>
+                    <ul>
+                        <D2Card title="Rock" img="rock.jpg" />
+                        <D2Card title="Pop" img="pop.jpg" />
+                        <D2Card title="Jazz" img="jazz.jpg" />
+                    </ul>
                 </div>
             </div>
         </div>
